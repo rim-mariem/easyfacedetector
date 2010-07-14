7363cc8e20392f8be15735825d9568a19ee9609d
Anton <Anton@localhost>
2010-07-14 08:06:43 +0300

    init

--- src/main.py
+++ src/main.py
@@ -1,22 +1,36 @@
 import Image
 
-if __name__ == '__main__':
+class PreImage (object):
+    
+    def __init__ (self, input_image_name):
+        self.input_image = Image.open(input_image_name)
+        self.width, self.height = self.input_image.size
+        size = self.input_image.size
+        self.output_color_space = Image.new("RGB", size)
+        self.output_grey_scale = Image.new("RGB", size)
+        print "\t==== class init ok ===="
+    
+    def convert (self, mode):
+        cbcr = [0,0,0]
+        for i in range(self.height):
+            for j in range (self.width):
+                rgb = self.input_image.getpixel((j,i))
+                cbcr[0] =  int(0.299*rgb[0] + 0.587*rgb[1] + 0.114*rgb[2]) #Y
+                cbcr[1] =  int((rgb[2] - cbcr[0])*0.564 + 128) #Cb
+                cbcr[2] =  int((rgb[0] - cbcr[0])*0.713 + 128) #Cr
+                self.output_color_space.putpixel((j, i),tuple(cbcr))
+        print "\t==== converting ok ===="
+        if mode == 's':
+            self.output_color_space.show()
+        elif mode == 'w':
+            self.output_color_space.save("lena4.jpg", "JPEG")
+        elif mode == 'sw':
+            self.output_color_space.show()
+            self.output_color_space.save("lena4.jpg", "JPEG")
+        else:
+            print "Invalid mode. mode mast be s, w, or sw"
+    
 
-    cbcr = [0,0,0]
-    input_image = Image.open("lena.jpg")
-    width, height = input_image.size
-    size = input_image.size
-    output_image = Image.new("RGB", size)
-    for i in range(height):
-        for j in range (width):
-            rgb = input_image.getpixel((j,i))
-            cbcr[0] =  int(0.299*rgb[0] + 0.587*rgb[1] + 0.114*rgb[2]) #Y
-            cbcr[1] =  int((rgb[2] - cbcr[0])*0.564 + 128) #Cb
-            cbcr[2] =  int((rgb[0] - cbcr[0])*0.713 + 128) #Cr
-            output_image.putpixel((j, i),tuple(cbcr))
-            
-    output_image.show()
-    output_image.save("lena4.jpg", "JPEG")
-    print "\t==== converting ok ===="
     
-    
+a = PreImage ("lena2.jpg")
+a.convert('sw')
