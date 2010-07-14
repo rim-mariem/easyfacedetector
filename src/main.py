00672ab899ab9ad3785db27ee9d9e346718820e4
aval <aval@localhost>
2010-07-14 09:45:48 +0300

    init

--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
 import Image
 
 class PreImage (object):
-    
+    '''
+        This class using for preparatin input image.
+        It convert RGB input image into some color 
+        space (YCbCr, HSV, etc). And then transform 
+        a color image from that color space into a 
+        grey scale image such that the gray value at 
+        each pixel shows the likelihood of the pixel 
+        belonging to the skin.
+        input_image - buffer for input image
+        width, height - sizes of input image
+        output_color_space - buffer for image which 
+                             contain some color space
+        RGB2YCbCr() - function for convert RGB into YCbCr 
+        convert() - function for convert from one color 
+                    space to another 
+                                
+    '''
     def __init__ (self, input_image_name):
         self.input_image = Image.open(input_image_name)
         self.width, self.height = self.input_image.size
@@ -9,15 +25,27 @@ class PreImage (object):
         self.output_color_space = Image.new("RGB", size)
         self.output_grey_scale = Image.new("RGB", size)
         print "\t==== class init ok ===="
+        
+    def RGB2YCbCr (self,rgb):
+        '''
+            convert RGB into YCbCr                                   
+        '''        
+        cbcr = [0,0,0]
+        cbcr[0] =  int(0.299*rgb[0] + 0.587*rgb[1] + 0.114*rgb[2]) #Y
+        cbcr[1] =  int((rgb[2] - cbcr[0])*0.564 + 128) #Cb
+        cbcr[2] =  int((rgb[0] - cbcr[0])*0.713 + 128) #Cr
+        return cbcr
     
     def convert (self, mode):
+        '''
+           convert() - function for convert from one color 
+                       space to another                                
+        '''    
         cbcr = [0,0,0]
         for i in range(self.height):
             for j in range (self.width):
                 rgb = self.input_image.getpixel((j,i))
-                cbcr[0] =  int(0.299*rgb[0] + 0.587*rgb[1] + 0.114*rgb[2]) #Y
-                cbcr[1] =  int((rgb[2] - cbcr[0])*0.564 + 128) #Cb
-                cbcr[2] =  int((rgb[0] - cbcr[0])*0.713 + 128) #Cr
+                cbcr = self.RGB2YCbCr(rgb)
                 self.output_color_space.putpixel((j, i),tuple(cbcr))
         print "\t==== converting ok ===="
         if mode == 's':
