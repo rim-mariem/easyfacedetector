95e88b5d44202b48bdfad700a4264277d76434b7
aval <aval@localhost>
2010-07-16 10:45:23 +0300

    init

--- src/PreImage.py
+++ src/PreImage.py
@@ -26,9 +26,11 @@ class PreImage (object):
         self.input_image = Image.open(input_image_name)
         self.width, self.height = self.input_image.size
         size = self.input_image.size
-        self.output_color_space = Image.new("RGB", size)
-        self.output_grey_scale = Image.new("RGB", size)
-        print "\t==== class init ok ===="
+        self.output_YCbCr_color_space = Image.new("RGB", size)
+        self.output_YCbCr_grey_scale = Image.new("L", size)
+        self.output_HSV_color_space= Image.new("L", size)
+        self.output_HSV_grey_scale = Image.new("L", size)
+        print "\t==== class PreImage init ok ===="
     #=============================================================    
     def RGB2YCbCr (self,rgb):
         '''
@@ -39,69 +41,69 @@ class PreImage (object):
         cbcr[1] =  int((rgb[2] - cbcr[0])*0.564 + 128) #Cb
         cbcr[2] =  int((rgb[0] - cbcr[0])*0.713 + 128) #Cr
         return cbcr
-     #=============================================================    
+    #=============================================================    
     def RGB2HSV (self,rgb):
         '''
             convert one pixel from RGB to HSV                                   
         '''        
-        hsv = [0,0,0]
+        hsv = [0,0.0,0.0]
         r,g,b = rgb
-        max = r
-        if max<g:
-            max = g
-        if max<b:
-            max=b
-        min = r
-        if min>g:
-            min = g
-        if min>b:
-            min=b
-        if max == 0:
-            hsv[1]=0
-            hsv[2]=0
-        else:
-            hsv[2]=max
-            hsv[1]=int((max-min)/max)
-        if (hsv[2] == r)&(g>=b):
-            #print max-min
-            hsv[0]= int((60*(g-b))/(hsv[1]+1))
-        elif (hsv[2] == r)&(g<b):
-            hsv[0]= int((60*(g-b))/(hsv[1]+1))+360  
-        elif hsv[2] == g:
-            hsv[0]= int((60*(b-r))/(hsv[1]+1))+120
-        else:
-            hsv[0]= int((60*(r-g))/(hsv[1]+1))+240
+        r=(float(r)/255.0)
+        g=(float(g)/255.0)
+        b=(float(b)/255.0)
+        maxc = max(r,g,b)
+        minc = min(r,g,b)
+        v = maxc
+        if minc==maxc: 
+            hsv[0]=0
+            hsv[1]=0.0
+            hsv[2]=v
+            return hsv 
+        s = (maxc-minc) / maxc
+        rc = (maxc-r) / (maxc-minc)
+        gc = (maxc-g) / (maxc-minc)
+        bc = (maxc-b) / (maxc-minc)
+        if r == maxc: 
+            h = bc-gc
+        elif g == maxc: 
+            h = 2.0+rc-bc
+        else: 
+            h = 4.0+gc-rc
+        h = (h/6.0) % 1.0
+        h=int(h*360)
         
-        hsv[0] = int(hsv[0]/2)
-        hsv[1] = hsv[1]*255
-        hsv[2] = hsv[2]*255
-        return hsv
-    #=============================================================    
-    def RGB2STOC (self,rgb):
-        '''
-            convert one pixel from RGB to chromatic color
-            space                                   
-        '''        
-        stoc = [0,0,0]
-        r,g,b = rgb
-        stoc[0] = r/(r+g+b)
-        stoc[1] = g/(r+g+b)
-        stoc[2] = 1 - stoc[0]-stoc[1]
-        
-        print rgb, stoc
-        return stoc
-     
+        hsv[0]=int(h-255)
+        if hsv[0]<0:
+            hsv[0]=0
+                    
+        hsv[1]=s
+        hsv[2]=v
+        return hsv 
     #=============================================================
     def YCbCr2GREY (self,rgb):
         '''
             convert one pixel from YCbCr to grey skin color model                                   
         '''        
+      
+        if ((rgb[1]>90)&(rgb[1]<150))&((rgb[2]>90)&(rgb[2]<150)):
+            grey = 0
+        else:
+            grey = 255
+        
+        
+        return grey      
+    #=============================================================
+    def HSV2GREY (self,rgb):
+        '''
+            convert one pixel from YCbCr to grey skin color model                                   
+        '''        
         grey = [0,0,0]
-        if ((rgb[1]>90)&(rgb[1]<170))&((rgb[2]>90)&(rgb[2]<170)):
+        print rgb
+        if rgb==0:
             grey = [255,255,255]
         else:
             grey = [0,0,0] 
-        return grey       
+        return grey        
     #=============================================================
     def convert_to_YCbCr (self):
         '''
@@ -112,9 +114,23 @@ class PreImage (object):
             for j in range (self.width):
                 rgb = self.input_image.getpixel((j,i))
                 cbcr = self.RGB2YCbCr(rgb)
-                self.output_color_space.putpixel((j, i),tuple(cbcr))
+                self.output_YCbCr_color_space.putpixel((j,i),tuple(cbcr))
+                                
+                rgb2=self.input_image.getpixel((j,i))
+                                       
+                self.YCbCr_to_grey_scale(j,i,cbcr)
+                
+                                             
         print "\t==== converting to YCbCr ok ===="
+        print "\t==== converting from YCbCr to grey scale ok ===="
+    
     #=============================================================
+    def YCbCr_to_grey_scale(self,x,y,rgb):
+              
+        grey = self.YCbCr2GREY(rgb)
+        self.output_YCbCr_grey_scale.putpixel((x,y),grey)
+        
+   #===============================================================    
     def convert_to_HSV (self):
         '''
            convert all image from RGB to HSV                                 
@@ -124,31 +140,23 @@ class PreImage (object):
             for j in range (self.width):
                 rgb = self.input_image.getpixel((j,i))
                 hsv = self.RGB2HSV(rgb)
-                self.output_color_space.putpixel((j, i),tuple(hsv))
+                self.output_HSV_color_space.putpixel((j, i),hsv[0])
+                self.HSV_to_grey_scale(j,i,hsv[0])
         print "\t==== converting to HSV ok ===="
-    #=============================================================
-    def convert_to_STOC (self):
-        '''
-           convert all image from RGB to HSV                                 
-        '''    
-        stoc = [0,0,0]
-        for i in range(self.height):
-            for j in range (self.width):
-                rgb = self.input_image.getpixel((j,i))
-                stoc = self.RGB2STOC(rgb)
-                self.output_color_space.putpixel((j, i),tuple(stoc))
-        print "\t==== converting to STOC ok ===="
+        print "\t==== converting from HSV to grey scale ok ===="
+   
     #===============================================================
-    def YCbCr_to_grey_scale (self):
+    def HSV_to_grey_scale(self,x,y,hue):
         '''
            convert all image from YCbCr to grey skin color model                                   
         '''    
-        for i in range(self.height):
-            for j in range (self.width):
-                rgb = self.input_image.getpixel((j,i))
-                grey = self.YCbCr2GREY(rgb)
-                self.output_grey_scale.putpixel((j, i),tuple(grey))
-        print "\t==== converting from YCbCr to grey scale ok ===="
+        temp = hue
+        if temp == 0:
+            temp = 255
+        else:
+            temp = 0
+        self.output_HSV_grey_scale.putpixel((x, y),temp)
+        
     #===============================================================
     def convert (self, mode, color_space):
         '''
@@ -157,12 +165,12 @@ class PreImage (object):
         if color_space == 'YCbCr':
             self.convert_to_YCbCr()
             if mode == 's':
-                self.output_color_space.show()
+                self.output_YCbCr_color_space.show()
             elif mode == 'w':
-                self.output_color_space.save("temp_cs.jpg", "JPEG")
+                self.output_YCbCr_color_space.save("temp_cs_YCbCr.jpg", "JPEG")
             elif mode == 'sw':
-                self.output_color_space.show()
-                self.output_color_space.save("temp_cs.jpg", "JPEG")
+                self.output_YCbCr_color_space.show()
+                self.output_YCbCr_color_space.save("temp_cs_YCbCr.jpg", "JPEG")
             elif mode == '':
                 print ""
             else:
@@ -170,29 +178,19 @@ class PreImage (object):
         elif color_space == 'HSV':
             self.convert_to_HSV()
             if mode == 's':
-                self.output_color_space.show()
+                self.output_HSV_color_space.show()
             elif mode == 'w':
-                self.output_color_space.save("temp_cs.jpg", "JPEG")
+                self.output_HSV_color_space.save("temp_cs_HSV.jpg", "JPEG")
             elif mode == 'sw':
-                self.output_color_space.show()
-                self.output_color_space.save("temp_cs.jpg", "JPEG")
+                self.output_HSV_color_space.show()
+                self.output_HSV_color_space.save("temp_cs_HSV.jpg", "JPEG")
             elif mode == '':
                 print ""
             else:
                 print "Invalid mode. mode mast be s, w, or sw"
         elif color_space == 'STOC':
-            self.convert_to_STOC()
-            if mode == 's':
-                self.output_color_space.show()
-            elif mode == 'w':
-                self.output_color_space.save("temp_cs.jpg", "JPEG")
-            elif mode == 'sw':
-                self.output_color_space.show()
-                self.output_color_space.save("temp_cs.jpg", "JPEG")
-            elif mode == '':
-                print ""
-            else:
-                print "Invalid mode. mode mast be s, w, or sw"
+            print "Stochastic model not supported now"
+            
         else: 
             print "Invalid color space. It most be YCbCr, HSV"
     #==========================================================
@@ -201,20 +199,30 @@ class PreImage (object):
             convert from some color space to grey skin color model                                  
         '''    
         if color_space == 'YCbCr':
-            self.YCbCr_to_grey_scale()
+            
             if mode == 's':
-                self.output_grey_scale.show()
+                self.output_YCbCr_grey_scale.show()
             elif mode == 'w':
-                self.output_grey_scale.save("temp_gr.jpg", "JPEG")
+                self.output_YCbCr_grey_scale.save("temp_gr_YCbCr.jpg", "JPEG")
             elif mode == 'sw':
-                self.output_grey_scale.show()
-                self.output_grey_scale.save("temp_gr.jpg", "JPEG")
+                self.output_YCbCr_grey_scale.show()
+                self.output_YCbCr_grey_scale.save("temp_gr_YCbCr.jpg", "JPEG")
             elif mode == '':
                 print ""
             else:
                 print "Invalid mode. mode mast be s, w, or sw"
         elif color_space == 'HSV':
-            print "HSV not supported now"
+            if mode == 's':
+                self.output_HSV_grey_scale.show()
+            elif mode == 'w':
+                self.output_HSV_grey_scale.save("temp_gr_HSV.jpg", "JPEG")
+            elif mode == 'sw':
+                self.output_HSV_grey_scale.show()
+                self.output_HSV_grey_scale.save("temp_gr_HSV.jpg", "JPEG")
+            elif mode == '':
+                print ""
+            else:
+                print "Invalid mode. mode mast be s, w, or sw"
         else: 
             print "Invalid color space. It most be YCbCr, HSV"
         
---
+++ src/SegmImage.py
@@ -0,0 +1,18 @@
+'''
+Created on 16.07.2010
+
+@author: aval
+'''
+
+class SegmImage(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        print "\t==== class SegmImage init ok ===="
+        
